67885569f303242224c9a4526e19e23c1e78236e
rslangl <fjellape1447@protonmail.com>
2025-06-28 20:17:06 +0200

    chore: simple RX signal, TX mattermost

--- bridge.sh
+++ bridge.sh
@@ -6,6 +6,13 @@ set -e
 : "${SIGNAL_GROUP_ID:?Env var SIGNAL_GROUP_ID not set, exiting}"
 : "${MATTERMOST_WEBHOOK_URL:?Env var MATTERMOST_WEBHOOK_URL not set, exiting}"
 
+echo "Starting bridge..."
+
 signal-cli --version
 
-echo "Bridge running!"
+signal-cli -u "$PHONE_NO" receive | while read -r line; do
+  echo "Sending to Mattermost: $line"
+  curl -X POST -H 'Content-Type: application/version' \
+    -d "{\"text\": \"$line\"}" \
+    "$MATTERMOST_WEBHOOK_URL"
+done
